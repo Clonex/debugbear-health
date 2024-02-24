d33f4db41e949dd9566cca01db904c2ae5d5a02a
Michael Salling <clonex_kontakt@hotmail.com>
2024-02-24 19:36:05 +0100

    Added pageId support

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,22 @@ import { cliArgs } from "valon-args";
 const options = cliArgs({
   debugBearToken: { type: "string", required: true },
   projectId: { type: "string", required: true },
+  pageId: { type: "string", required: true },
 });
 
 const debugBear = new DebugBear(options.debugBearToken);
 const project = await debugBear.projects.get(options.projectId);
+const page = project.pages.find((page) => page.id === options.pageId);
 
-console.log(project);
+if (!page) {
+  console.log(
+    "Available pages",
+    project.pages.map((page) => ({
+      id: page.id,
+      name: page.name,
+    }))
+  );
+  throw new Error("Page not found!");
+}
+
+console.log(page);
