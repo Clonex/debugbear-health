95fe479ef5f2789c5f4f9c2aaf8e8e1c61854c66
Clonex <clonex_kontakt@hotmail.com>
2024-03-08 10:41:11 +0100

    Added gg script

---
+++ src/guloggratis.ts
@@ -0,0 +1,25 @@
+import { cliArgs } from "valon-args";
+import { run } from "./run.js";
+
+const options = cliArgs({
+  debugBearToken: { type: "string", required: true },
+});
+const projectId = "29163";
+
+const sections = {
+  "desktop-full-load": "54392",
+  "desktop-no-3rd-party": "54394", // "57633",
+
+  "mobile-full-load": "54402",
+  "mobile-no-3rd-party": "54401",
+};
+
+const averages = await Promise.all(
+  Object.entries(sections).map(async ([name, pageId]) => {
+    const { average } = await run(options.debugBearToken, projectId, pageId);
+
+    return `${name}: ${(average * 100).toFixed(2)}`;
+  })
+);
+
+console.log(averages.join("\n"));
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-import { DebugBear } from "debugbear";
 import { cliArgs } from "valon-args";
-import { daysAgo, getMeasurements } from "./utils.js";
+import { run } from "./run.js";
 
 const options = cliArgs({
   debugBearToken: { type: "string", required: true },
@@ -8,28 +7,9 @@ const options = cliArgs({
   pageId: { type: "string", required: true },
 });
 
-const debugBear = new DebugBear(options.debugBearToken);
-const project = await debugBear.projects.get(options.projectId);
-const page = project.pages.find((page) => page.id === options.pageId);
-
-const testPage = await debugBear.pages.getMetrics(options.pageId, {
-  from: daysAgo(1),
-  to: new Date(),
-});
-
-const metrics = await getMeasurements(debugBear, options.pageId, 14);
-if (!page) {
-  console.log(
-    "Available pages",
-    project.pages.map((page) => ({
-      id: page.id,
-      name: page.name,
-    }))
-  );
-  throw new Error("Page not found!");
-}
-
-const average =
-  metrics.reduce((sum, metric) => sum + metric.score, 0) / metrics.length;
-
+const { average } = await run(
+  options.debugBearToken,
+  options.projectId,
+  options.pageId
+);
 console.log(average);
